e3a86c387b4da3ffabb0407ebe41fc0cd2b53255
rohithe401-star <rohithe401@gmail.com>
2026-06-15 14:13:16 +0530

    Create StudyRequest.java

---
+++ src/StudyRequest.java
@@ -0,0 +1,15 @@
+package com.research.ai.assistant;
+
+import lombok.Data;
+
+@Data
+public class StudyRequest {
+    private String content;
+    private String operation;
+    private String researchGoal;
+    private String ProgrammingLanguage;
+    private String subject;
+    private String studyTopic;
+
+}
+
